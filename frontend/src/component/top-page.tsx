97b131059e595d10c794438884e8e914c9399c59
tomtsutom <40418321+Tsutomu-Ikeda@users.noreply.github.com>
2022-09-22 06:59:59 +0900

    update currentMonth

--- frontend/src/component/top-page.tsx
+++ frontend/src/component/top-page.tsx
@@ -19,7 +19,7 @@ type CommuteStats = {
 };
 
 export const TopPage: FC = () => {
-  const currentMonth = "2022-08";
+  const currentMonth = "2022-09";
   const redirectWaitSeconds = 1;
   const { token } = useLogin();
   const {
