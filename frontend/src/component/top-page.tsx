b43f34086dcd358727866f17eae3c026c6de5d7d
Tsutomu-Ikeda <40418321+Tsutomu-Ikeda@users.noreply.github.com>
2022-12-05 22:07:27 +0900

    Update currentMonth

--- frontend/src/component/top-page.tsx
+++ frontend/src/component/top-page.tsx
@@ -19,7 +19,7 @@ type CommuteStats = {
 };
 
 export const TopPage: FC = () => {
-  const currentMonth = "2022-10";
+  const currentMonth = "2022-11";
   const redirectWaitSeconds = 1;
   const { token } = useLogin();
   const {
