f08a1bc3ecb1940be6bb670b885e50320c2721b3
tomtsutom <40418321+Tsutomu-Ikeda@users.noreply.github.com>
2023-03-20 00:11:16 +0900

    displaySizeが初回ロード時にpcが優先されてしまう問題に対処

--- frontend/src/hooks/use-display-size.ts
+++ frontend/src/hooks/use-display-size.ts
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react"
 
 export const useDisplaySize = () => {
-  const mediaQuery = '(min-width: 880px)'
-  const [displaySize, setDisplaySize] = useState<"pc" | "sp">("pc");
+  const mediaQuery = "(min-width: 880px)"
+  const getDisplaySize = () => window.matchMedia && window.matchMedia(mediaQuery).matches ? "pc" : "sp"
+  const [displaySize, setDisplaySize] = useState<"pc" | "sp">(getDisplaySize());
 
   useEffect(() => {
-    const update = () => { setDisplaySize(window.matchMedia && window.matchMedia(mediaQuery).matches ? "pc" : 'sp') }
+    const update = () => { setDisplaySize(getDisplaySize()) }
     update();
     window.matchMedia(mediaQuery).addEventListener("change", update)
     return () => {
